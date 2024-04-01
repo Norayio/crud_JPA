1f1d2a726b4dd5c6e3cbe119b1366aa6503beeea
Norayio <pierresediaz@gmail.com>
2024-04-01 16:33:46 -0500

    commit de apertura y creación de paquete vista y lógica. JFrame culminado.

---
+++ src/main/java/com/mycompany/crudjpa/CrudJPA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crudjpa;
+
+/**
+ *
+ * @author Norayio
+ */
+public class CrudJPA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
